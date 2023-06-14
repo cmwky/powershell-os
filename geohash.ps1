856a60e2b133ff57a36ffaa603c05bd281d218b3
Charles W <76116118+cmwky@users.noreply.github.com>
2023-06-14 17:38:11 -0400

    Create geohash.ps1

---
+++ geohash.ps1
@@ -0,0 +1,37 @@
+function Convert-GeohashToLatLng {
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]$geohash
+    )
+
+    $base32 = "0123456789bcdefghjkmnpqrstuvwxyz"
+    $lon = @(-180, 180)
+    $lat = @(-90, 90)
+
+    $isEven = $true
+    $geohash.ToCharArray() | ForEach-Object {
+        $c = $base32.IndexOf($_)
+
+        for ($i=4; $i -ge 0; $i--) {
+            $mask = [math]::Pow(2, $i)
+            if ($isEven) {
+                if (($c -band $mask) -ne 0) {
+                    $lon[0] = ($lon[0] + $lon[1]) / 2
+                } else {
+                    $lon[1] = ($lon[0] + $lon[1]) / 2
+                }
+            } else {
+                if (($c -band $mask) -ne 0) {
+                    $lat[0] = ($lat[0] + $lat[1]) / 2
+                } else {
+                    $lat[1] = ($lat[0] + $lat[1]) / 2
+                }
+            }
+            $isEven = !$isEven
+        }
+    }
+    return @( (($lat[0] + $lat[1]) / 2), (($lon[0] + $lon[1]) / 2 ))
+}
+
+# Call the function
+Convert-GeohashToLatLng -geohash "dngbmjzg"
