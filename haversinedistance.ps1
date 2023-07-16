984cab8ff0f73a3bb7891fa5ec2dfb45ab3c142c
Charles W <charlesmwhitehead@gmail.com>
2023-07-15 23:26:10 -0400

    adds haversine distance script

---
+++ haversinedistance.ps1
@@ -0,0 +1,31 @@
+﻿function ConvertTo-Radian($degree) {
+    return $degree * [Math]::PI / 180
+}
+
+function Get-HaversineDistance($lat1, $long1, $lat2, $long2) {
+    # Define Earth's radius in miles
+    $earthRadius = 3959
+
+    $dLat = ConvertTo-Radian ($lat2 - $lat1)
+    $dLong = ConvertTo-Radian ($long2 - $long1)
+
+    $a = [Math]::Sin($dLat / 2) * [Math]::Sin($dLat / 2) +
+         [Math]::Cos((ConvertTo-Radian $lat1)) * [Math]::Cos((ConvertTo-Radian $lat2)) *
+         [Math]::Sin($dLong / 2) * [Math]::Sin($dLong / 2)
+
+    $c = 2 * [Math]::Atan2([Math]::Sqrt($a), [Math]::Sqrt(1-$a))
+
+    return $earthRadius * $c
+}
+
+#Eiffel Tower
+$lat1 = 48.8584
+$long1 = 2.2945
+
+#Statue of Liberty
+$lat2 = 40.6892
+$long2 = 74.0445
+
+$distance = Get-HaversineDistance $lat1 $long1 $lat2 $long2
+
+Write-Output "The distance between the two points is $($distance) miles."
