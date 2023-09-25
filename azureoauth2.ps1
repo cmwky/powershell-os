25a3d114462b26d8bafa94e9c5a2a8e27293189a
Charles <charlesmwhitehead@gmail.com>
2023-09-25 14:05:37 -0400

    updates azure oauth + readme

--- azureoauth2.ps1
+++ azureoauth2.ps1
@@ -1,33 +1,30 @@
-# Assuming you store your variables in a hash table or somewhere similar. Here's an example:
-$variables = @{
-    "bearerToken" = $null
-    "bearerTokenExpiresOn" = 0
-    "tenantId" = "YOUR_TENANT_ID"
-    "clientId" = "YOUR_CLIENT_ID"
-    "clientSecret" = "YOUR_CLIENT_SECRET"
-    "resource" = "https://management.azure.com/"
-}
+function Get-AzureBearerToken {
+    param (
+        [Parameter(Mandatory=$true)][string]$tenantId,
+        [Parameter(Mandatory=$true)][string]$clientId,
+        [Parameter(Mandatory=$true)][string]$clientSecret,
+        [Parameter(Mandatory=$false)][string]$resource = "https://management.azure.com/"
+    )
 
-# Check if the token needs to be refreshed
-if (-not $variables["bearerToken"] -or (Get-Date).ToUniversalTime().Ticks -gt [DateTime]::FromBinary($variables["bearerTokenExpiresOn"]).Ticks) {
-
-    # Prepare the body for the request
     $body = @{
         grant_type    = "client_credentials"
-        client_id     = $variables["clientId"]
-        client_secret = $variables["clientSecret"]
-        resource      = $variables["resource"]
+        client_id     = $clientId
+        client_secret = $clientSecret
+        resource      = $resource
     }
 
-    # Make the request
     try {
-        $response = Invoke-RestMethod -Uri "https://login.microsoftonline.com/$($variables['tenantId'])/oauth2/token" -Method Post -Body $body -ContentType "application/x-www-form-urlencoded"
+        $response = Invoke-RestMethod -Uri "https://login.microsoftonline.com/$tenantId/oauth2/token" `
+                                      -Method Post `
+                                      -Body $body `
+                                      -ContentType "application/x-www-form-urlencoded"
         
-        # If the request is successful, set the token and expiry variables
-        $variables["bearerToken"] = $response.access_token
-        $variables["bearerTokenExpiresOn"] = (Get-Date).AddSeconds($response.expires_on).ToUniversalTime().Ticks
+        $token = $response.access_token
+        $expiresOn = (Get-Date).AddSeconds($response.expires_on).ToUniversalTime().Ticks
+        return $token, $expiresOn
 
     } catch {
         Write-Error $_.Exception.Message
+        return $null, $null
     }
 }
